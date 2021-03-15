cfee8cf92d237e357054a6399ab2a14078956d9c
jay_fighting <jay_fighting@hotmail.com>
2021-03-15 14:20:49 -0700

    Added result/envelope return type

---
+++ Source/API/LD.DocMan.API/Controllers/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using CSharpFunctionalExtensions;
+using LD.DocMan.API.Utils;
+using LD.DocMan.Domain.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LD.DocMan.API.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected new IActionResult Ok()
+        {
+            return base.Ok(Envelope.Ok());
+        }
+
+        protected IActionResult Ok<T>(T result)
+        {
+            return base.Ok(Envelope.Ok(result));
+        }
+
+        protected IActionResult FromResult(Result result)
+        {
+            if (result.IsSuccess)
+            {
+                return Ok();
+            }
+
+            if (result.Error == Errors.General.NotFound().Code.ToString())
+            {
+                return NotFound(Envelope.Error(result.Error));
+            }
+
+            return BadRequest(Envelope.Error(result.Error));
+        }
+    }
+}
